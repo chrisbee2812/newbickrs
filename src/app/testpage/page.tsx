dcc6d53d999d67d35950b0b3577f37b70a6e0208
Christopher Bicknell <chrisb2812@gmail.com>
2025-09-17 14:31:53 +0100

    undone nextseo

---
+++ src/app/testpage/page.tsx
@@ -0,0 +1,14 @@
+// pages/test.js
+// import { NextSeo } from 'next-seo';
+
+const Test = () => (
+  <>
+    {/* <NextSeo
+      title="Simple Usage Example"
+      description="A short description goes here."
+    /> */}
+    <p>Simple Usage</p>
+  </>
+);
+
+export default Test;
