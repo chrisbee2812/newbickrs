26d695e2b2c94dd28d9be45d354a93686a3c35b5
Christopher Bicknell <chrisb2812@gmail.com>
2026-04-08 09:22:36 +0100

    sorting static files for ovhcloud export

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
@@ -19,5 +17,7 @@ const nextConfig: NextConfig = {
     ],
   },
 };
+
+module.exports = nextConfig;
 
 export default nextConfig;
